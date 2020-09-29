3c958a9431a3d05de1ee85a0699fc72c0a708137
Ziqing Lu <45545275+ziqlu0722@users.noreply.github.com>
2020-09-29 14:16:07 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config():
     # SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
     # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] or "postgresql://localhost/webapp_db"
@@ -9,5 +11,5 @@ class Config():
 
 class TestConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/webapp_test_db"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'webapp_test_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
