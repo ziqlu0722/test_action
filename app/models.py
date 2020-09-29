6631bb5c7b5ff9c45d998fd8d195c9b84da3a858
Ziqing Lu <45545275+ziqlu0722@users.noreply.github.com>
2020-09-29 14:51:01 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,44 +5,46 @@ import re
 import uuid
 
 # this is to create UUID for sqlite
-from sqlalchemy.types import TypeDecorator, CHAR
+# from sqlalchemy.types import TypeDecorator, CHAR
 
-class GUID(TypeDecorator):
-    """Platform-independent GUID type.
-    Uses PostgreSQL's UUID type, otherwise uses
-    CHAR(32), storing as stringified hex values.
-    """
-    impl = CHAR
+# class GUID(TypeDecorator):
+#     """Platform-independent GUID type.
+#     Uses PostgreSQL's UUID type, otherwise uses
+#     CHAR(32), storing as stringified hex values.
+#     """
+#     impl = CHAR
 
-    def load_dialect_impl(self, dialect):
-        if dialect.name == 'postgresql':
-            return dialect.type_descriptor(UUID())
-        else:
-            return dialect.type_descriptor(CHAR(32))
+#     def load_dialect_impl(self, dialect):
+#         if dialect.name == 'postgresql':
+#             return dialect.type_descriptor(UUID())
+#         else:
+#             return dialect.type_descriptor(CHAR(32))
 
-    def process_bind_param(self, value, dialect):
-        if value is None:
-            return value
-        elif dialect.name == 'postgresql':
-            return str(value)
-        else:
-            if not isinstance(value, uuid.UUID):
-                return "%.32x" % uuid.UUID(value).int
-            else:
-                # hexstring
-                return "%.32x" % value.int
+#     def process_bind_param(self, value, dialect):
+#         if value is None:
+#             return value
+#         elif dialect.name == 'postgresql':
+#             return str(value)
+#         else:
+#             if not isinstance(value, uuid.UUID):
+#                 return "%.32x" % uuid.UUID(value).int
+#             else:
+#                 # hexstring
+#                 return "%.32x" % value.int
 
-    def process_result_value(self, value, dialect):
-        if value is None:
-            return value
-        else:
-            if not isinstance(value, uuid.UUID):
-                value = uuid.UUID(value)
-            return value
+#     def process_result_value(self, value, dialect):
+#         if value is None:
+#             return value
+#         else:
+#             if not isinstance(value, uuid.UUID):
+#                 value = uuid.UUID(value)
+#             return value
 
 class User(db.Model):
 #     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    id = db.Column(GUID(), primary_key=True, default=str(uuid.uuid4()), nullable=False)
+#     id = db.Column(GUID(), primary_key=True, default=str(uuid.uuid4()), nullable=False)
+
+    id = db.Column(db.Integer, primary_key=True)
     email_address = db.Column(db.String(120), unique=True, nullable=False)
     first_name = db.Column(db.String(60), nullable=False)
     last_name = db.Column(db.String(60), nullable=False)
